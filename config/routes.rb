d380915ff5faa11c7660a192bfe506124ba0385f
jo-endo <enjoooooo@icloud.com>
2020-08-24 08:26:25 +0900

    group-operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "group#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
